9e6568101733f08acb7c34e5584e4389e81edfa3
Twinegarner <42616332+Twinegarner@users.noreply.github.com>
2021-02-26 15:28:59 -0800

    2/26/2021
    added the outline for quest going to work on location item and enemy quest

---
+++ QuestManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public QuestObject[] quests;// a holder for the quest array
+    public bool[] questCompleted;//keeps track of completed quest
+
+    public DialogueManager theDM;//the dialogue manager for dialogue
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questCompleted = new bool[questCompleted.Length];//make an array the size of quests
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showQuestText(string questText)//function that uses manager for text
+    {
+        theDM.dialogueLines = new string[1];//remake the array
+        theDM.dialogueLines[0] = questText;//add the quest text
+
+        theDM.currentLine = 0;//reset the line 
+        theDM.ShowDialogue();//display the box
+
+    }
+}
---
+++ QuestObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour
+{
+    public int questNumber;//keeps track of the quest number/order
+    public QuestManager theQM;//refrance to the quest manager
+
+    public string startText;//the start text for the quest
+    public string endText;//the end text for the quest
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartQuest()
+    {
+        theQM.showQuestText(startText);//pass the start text the qm
+
+    }
+
+    public void EndQuest()
+    {
+        theQM.showQuestText(endText);
+        theQM.questCompleted[questNumber] = true;//set to  finish the quest
+        gameObject.SetActive(false);//deactivate the quest when done 
+    }
+}
