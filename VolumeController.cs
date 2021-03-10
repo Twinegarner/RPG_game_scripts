593fcbd14bd96e7dc8185ef1197497e5f4413a06
Twinegarner <42616332+Twinegarner@users.noreply.github.com>
2021-03-10 12:55:35 -0800

    3/10/2021 volume manger update

---
+++ VolumeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{
+    private AudioSource theAudio;//this connects to all audio
+    private float audioLevel;//the min and max level of audio
+    public float defaultAudio = 1;//this is the starting audio level (default is 1)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio = GetComponent<AudioSource>();//get the audio and attach
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetAudioLevel(float volume)//set the volume level manually
+    {
+        if(theAudio == null)//quick fix to stop this script from running first 
+        {
+            theAudio = GetComponent<AudioSource>();//get the audio and attach
+        }
+
+        audioLevel = volume * defaultAudio;//set the volume
+        theAudio.volume = audioLevel;//set the audio levels
+    }
+}
---
+++ VolumeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour
+{
+    public VolumeController[] vcObjects;//holds all the audio controller objects
+    public float maxVolumeLevel = 1.0f;//max volume
+    public float currentVolumeLevel;//the current volume levels
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vcObjects = FindObjectsOfType<VolumeController>();//set up refrance
+        if(currentVolumeLevel > maxVolumeLevel)//safty net for volume
+        {
+            currentVolumeLevel = maxVolumeLevel;
+        }
+
+        for(int i = 0; i < vcObjects.Length; i++)//loop through array
+        {
+            vcObjects[i].SetAudioLevel(currentVolumeLevel);//set the audio levels
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
