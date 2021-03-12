8c9c543804c422abc7705a73ac26698a0a04c256
Twinegarner <42616332+Twinegarner@users.noreply.github.com>
2021-03-12 14:33:42 -0800

    3/12/2021 money update gold gold gold

---
+++ GoldPickUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPickUp : MonoBehaviour
+{
+    public int value;//the value of the coin
+    public MoneyManager theMM;// the money manager
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theMM = FindObjectOfType<MoneyManager>();//find the money manager
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D other)//when the player enters the coins collider
+    {
+        if(other.gameObject.name == "Player")//if the player walks over it collect it
+        {
+            theMM.addMoney(value);//add the value of the coin
+            Destroy(gameObject);//get rid of coin
+
+        }
+    }
+}
---
+++ MoneyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public Text moneyText;//the text to tell the player how much money they have
+    public int currentGold;//the value that the player has currently
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentGold = 0;//on start the player has zero gold
+
+        moneyText.text = "Gold: " + currentGold;//set the text for starting game
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    public void addMoney(int goldToAdd)//public function to add and update the HUD
+    {
+        currentGold += goldToAdd;//add up the gold
+        moneyText.text = "Gold: " + currentGold;//set the text for starting game
+    }
+}
