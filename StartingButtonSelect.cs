9b29d40587ab572d328fde62411a99a81df64aa1
Twinegarner <42616332+Twinegarner@users.noreply.github.com>
2021-03-22 14:01:49 -0700

    3/22/2021 starting select button

---
+++ StartingButtonSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartingButtonSelect : MonoBehaviour
+{
+    public Button button;//the button to select on start or transtion
+    public bool awakeOnStart;//dose the button awake on load
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (awakeOnStart)//if awake is press then once on start
+        {
+            button.Select();
+        }
+    }
+
+   public void selectTrigger(Button other)//trigger the select button NOTE: only for button actions
+    {
+        other.Select();
+    }
+}
